fc7b0a4ee722f80eb7ee13b55b4f730c011b8ba0
aaaaeeeer <51405743+aaaaeeeer@users.noreply.github.com>
2019-11-06 17:24:12 +0800

    注册成功

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './router'
-import App from "./pages/register"
+import App from './router'
 import axios from './utils/axios';
 import * as serviceWorker from './serviceWorker';
 
